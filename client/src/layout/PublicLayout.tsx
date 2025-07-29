96777d0c65a37945ee27a8cfe1898835737d0ce6
pranjalpandey693 <pranjalpandey693@gmail.com>
2025-07-29 05:37:34 +0530

    changed logic for data

--- client/src/layout/PublicLayout.tsx
+++ client/src/layout/PublicLayout.tsx
@@ -3,18 +3,14 @@ import Navigationbar from '@/components/Nav/navigationbar';
 import { Outlet } from 'react-router-dom';
 
 
-import type { User } from '@/interfaces';
+
 
 
 const PublicLayout = () => {
-     const testuser :User = {
-        name : 'jon don',
-        email: 'jondon@gmail.com',
-        isadmin:false
-      }
+    
   return (
     <div className='flex flex-col min-h-screen'>
-      <Navigationbar user={testuser}/>
+      <Navigationbar />
       <main className='flex-grow'>
         <Outlet/>
       </main>
