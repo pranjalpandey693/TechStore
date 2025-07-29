3beef1f95e5113e374a19cda4328b06b61b8d31b
pranjalpandey693 <pranjalpandey693@gmail.com>
2025-07-29 06:23:49 +0530

    correct api endpoint

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -3,6 +3,6 @@ import axios from 'axios'
 
 export const API = axios.create ({
     
-    baseURL:import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+    baseURL:import.meta.env.VITE_API_URL || "http://localhost:3001/api",
     withCredentials: true,
 })
