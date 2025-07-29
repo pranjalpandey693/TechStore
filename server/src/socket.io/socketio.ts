3e15bb97f9cbe493ff22b034383a2fadf43ea065
pranjalpandey693 <pranjalpandey693@gmail.com>
2025-07-29 06:23:34 +0530

    fixed cors error

--- server/src/socket.io/socketio.ts
+++ server/src/socket.io/socketio.ts
@@ -3,7 +3,8 @@ import http from "http"
 import app from "../app"
 
 const server = http.createServer(app)
-const io = new Server(server,{cors:{origin:"*"}})
+const io = new Server(server,{cors:{ origin: "http://localhost:5173",
+  credentials: true,}})
 
 io.on("connection",(socket)=>{
     console.log("a user connected",socket.id);
