c09fa621bd0a61314215176135cc1bc621464e29
pranjalpandey693 <pranjalpandey693@gmail.com>
2025-07-31 03:06:09 +0530

    fixed css

--- client/src/layout/Auth/Authlayout.tsx
+++ client/src/layout/Auth/Authlayout.tsx
@@ -3,8 +3,9 @@ import { Outlet } from 'react-router-dom';
 
 const Authlayout = () => {
   return (
-    <div className='flex items-center justify-center min-h-screen bg-gray-100'>
-      <div className='w-full max-w-md p-8 bg-white rounded shadow'>
+    <div className='flex items-center justify-center min-h-screen  bg-gray-100'>
+      <div className='w-full max-w-md  p-8  '>
+              
          <Outlet/>
       </div>
     </div>
