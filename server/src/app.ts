5d882b1583677799a0856bb46999e313c632852b
pranjalpandey693 <pranjalpandey693@gmail.com>
2025-09-18 10:14:33 +0530

    edit

--- server/src/app.ts
+++ server/src/app.ts
@@ -12,10 +12,20 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-    origin: "*",
-  credentials: true,
-}));
+
+const allowedOrigins = ["https://tech-store-lemon-beta.vercel.app","https://localhost","http://localhost:5173"];
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
+
 app.use(cookieParser())
 
 app.use("/api/auth", authRoutes);
