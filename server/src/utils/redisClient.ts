c194f089e3083b674e7c48dc17336812a2024ff7
pranjalpandey693 <pranjalpandey693@gmail.com>
2025-09-18 09:39:22 +0530

    edit

--- server/src/utils/redisClient.ts
+++ server/src/utils/redisClient.ts
@@ -1,13 +1,11 @@
 import Redis from "ioredis";
 
-const redisHost: string = process.env.REDIS_URL || "127.0.0.1"
+const redisUrl = process.env.REDIS_URL || "redis://127.0.0.1:6379";
 
-const redisClient = new Redis({
-    host: redisHost,
-    port:6379,
-})
 
-redisClient.on("connect",()=>console.log("redis connected"))
-redisClient.on("error",(err)=>console.error("redis error:",err))
+const redisClient = new Redis(redisUrl);
 
-export default redisClient
+redisClient.on("connect", () => console.log("Redis connected"));
+redisClient.on("error", (err) => console.error("redis error:", err));
+
+export default redisClient;
