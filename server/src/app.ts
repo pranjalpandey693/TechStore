c87e0ecb99f690ebb92a10beb8fd0535817e7921
pranjalpandey693 <pranjalpandey693@gmail.com>
2025-09-18 10:04:47 +0530

    edit

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,8 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
 import { authRoutes, cartRoutes, productRoutes } from "./routes";
-import cookieParser from "cookie-parser";
-import cors from "cors";
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
 
 dotenv.config();
 connectDB();
@@ -11,27 +12,15 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-const allowedOrigins = ["http://localhost", "http://localhost:5173"];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      } else {
-        return callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
-
-app.use(cookieParser());
+app.use(cors({
+    origin: "*",
+  credentials: true,
+}));
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
-app.use("/api/cart", cartRoutes);
+app.use("/api/cart",cartRoutes)
 
 //const PORT = process.env.PORT || 3001;
 
