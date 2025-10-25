19ff0c38fe760834f1ef78fc3d8986fdef55492e
pranjalpandey693 <pranjalpandey693@gmail.com>
2025-10-25 13:27:43 +0530

    corrected indentation

--- client/src/routes/ProtectedRoutes.tsx
+++ client/src/routes/ProtectedRoutes.tsx
@@ -1,26 +1,21 @@
-import { useAuth } from "@/hooks/useAuth"
-import { Navigate, Outlet } from "react-router-dom"
-
-
+import { useAuth } from "@/hooks/useAuth";
+import { Navigate, Outlet } from "react-router-dom";
 
 interface ProtectedRouteProps {
-    allowedRoles?: Array<"admin"| "seller" | "customer">
+  allowedRoles?: Array<"admin" | "seller" | "customer">;
 }
 
-const ProtectedRoute: React.FC<ProtectedRouteProps> = ({allowedRoles})=>{
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ allowedRoles }) => {
+  const { user, isAuthenticated } = useAuth();
 
-const {user,isAuthenticated} = useAuth()
+  if (isAuthenticated) {
+    return <Navigate to="/auth/login" replace />;
+  }
+  if (allowedRoles && !allowedRoles.includes(user!.role!)) {
+    return <Navigate to="/" replace />;
+  }
 
-if(isAuthenticated){
-    return <Navigate to="/auth/login" replace  />
-}
-if(allowedRoles && !allowedRoles.includes(user!.role!)){
-    return <Navigate to="/" replace />
-}
+  return <Outlet />;
+};
 
-
-
- return <Outlet/>
-}
-
-export default ProtectedRoute
+export default ProtectedRoute;
