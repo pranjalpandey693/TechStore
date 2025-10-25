1aa83bc2b23835adc49a6a0cd00612e962dccf0b
pranjalpandey693 <pranjalpandey693@gmail.com>
2025-10-25 21:34:54 +0530

    fixed toast double render issue

--- client/src/routes/ProtectedRoutes.tsx
+++ client/src/routes/ProtectedRoutes.tsx
@@ -1,5 +1,7 @@
 import { useAuth } from "@/hooks/useAuth";
+import { useRef } from "react";
 import { Navigate, Outlet } from "react-router-dom";
+import { toast } from "sonner";
 
 interface ProtectedRouteProps {
   allowedRoles?: Array<"admin" | "seller" | "customer">;
@@ -7,11 +9,16 @@ interface ProtectedRouteProps {
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ allowedRoles }) => {
   const { user, isAuthenticated } = useAuth();
+  const toastShown = useRef(false)
 
   if (!isAuthenticated && !user) {
     return <Navigate to="/auth/login" replace />;
   }
   if (allowedRoles && !allowedRoles.includes(user!.role!)) {
+    if(!toastShown.current){
+      toast.error("Access denied")
+      toastShown.current = true
+    }
     return <Navigate to="/" replace />;
   }
 
