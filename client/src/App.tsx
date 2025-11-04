9483138a140a86485cfcfd6a36893398a1b7f9da
pranjalpandey693 <pranjalpandey693@gmail.com>
2025-11-05 03:57:12 +0530

    removed mutiple hook call

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,10 @@
 import useAuthInitializer from "./hooks/useAuthInitializer";
-import useFetchData from "./hooks/useFetchData";
 import Routes from "./routes";
 
 
 const App = () => {
     useAuthInitializer()
-    useFetchData()
+    
   return (
    
     
