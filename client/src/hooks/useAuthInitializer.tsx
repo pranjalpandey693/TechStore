2f5f4781941ec9c2bc44b6b0ff5a1272f009b124
pranjalpandey693 <pranjalpandey693@gmail.com>
2025-11-05 03:57:42 +0530

    merged intial calls

--- client/src/hooks/useAuthInitializer.tsx
+++ client/src/hooks/useAuthInitializer.tsx
@@ -1,32 +1,36 @@
-import { getCurrentUser, logoutUser, refreshToken } from "@/redux/slices/authSlice";
+import {
+  getCurrentUser,
+  logoutUser,
+  refreshToken,
+} from "@/redux/slices/authSlice";
+import { fetchCart } from "@/redux/slices/CartSlice";
+import { fetchProducts } from "@/redux/slices/productSlice";
 import type { AppDispatch } from "@/redux/store";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 
-
-
 const useAuthInitializer = () => {
-    const dispatch = useDispatch<AppDispatch>()
-       
-    useEffect(()=>{
-       const initializeAuth = async ()=>{
-       
+  const dispatch = useDispatch<AppDispatch>();
 
+  useEffect(() => {
+    const initializeAuth = async () => {
+      try {
+        await dispatch(getCurrentUser()).unwrap();
+
+        dispatch(fetchProducts({})).unwrap();
+
+        dispatch(fetchCart()).unwrap();
+      } catch (error) {
         try {
-            await dispatch(getCurrentUser()).unwrap()
-            
-        } catch (error) {
-            try {
-                await dispatch(refreshToken()).unwrap()
-                await dispatch(getCurrentUser()).unwrap()
-            } catch (refreshError) {
-                dispatch(logoutUser())
-            }
+          await dispatch(refreshToken()).unwrap();
+          await dispatch(getCurrentUser()).unwrap();
+        } catch (refreshError) {
+          dispatch(logoutUser());
         }
-       }
-       initializeAuth()
-    },[dispatch])
-  
-}
+      }
+    };
+    initializeAuth();
+  }, [dispatch]);
+};
 
 export default useAuthInitializer;
